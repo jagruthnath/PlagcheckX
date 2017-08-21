527329c4eaea72ab58fafd4ad3f335f1f396eed4
jagruthnath <jagruthnath@users.noreply.github.com>
2017-08-21 14:46:48 +0530

    Almost had it

--- BagOfWords.py
+++ BagOfWords.py
@@ -1,3 +1,5 @@
+import math
+
 f1=open("Testfiles\/f1.py", "r")
 f2=open("Testfiles\/f2.py", "r")
 str=f1.read().lower()
@@ -13,10 +15,28 @@ for x in a:
         pass
     else:
         s=x[:-1]
-    d1[s]=a.count(x)
+    d1[s] = a.count(x)
+    d2[s] = 0
 for x in b:
     if 123 > ord(x[len(x) - 1]) > 96 or 58 > ord(x[len(x) - 1]) > 47 or ord(x[len(x) - 1]) == 95:
         pass
     else:
-        s=x[:-1]
-    d1[s]=a.count(x)
+        s = x[:-1]
+    d2[s] = b.count(x)
+    if s not in d1:
+        d1[s]=0
+eu1=0
+eu2=0
+for x in d1:
+    eu1 += d1[x]**2
+for x in d2:
+    eu2 += d2[x]**2
+eu1 = math.sqrt(eu1)
+eu2 = math.sqrt(eu2)
+c=a+b
+dp=0
+print(d1,d2)
+for x in c:
+    dp += ( d1[x] * d2[x] )
+sim = dp / ( eu1 * eu2 )
+print( sim*100 , "% match")
