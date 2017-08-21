92d9232d38eaa8b34cbf2237a9413499c278ff89
jagruthnath <jagruthnath@users.noreply.github.com>
2017-08-21 12:05:11 +0530

    File opened. Comparision of words running perfectly.(Bug fixes ) Plagiarism % rounded

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
-f1=open("Testfiles\j1.txt", "r")
-f2=open("Testfiles\j2.txt", "r")
+f1=open("Testfiles\/f1.py", "r")
+f2=open("Testfiles\/f2.py", "r")
 str=f1.read()
 str2=f2.read()
 a=str.split()
@@ -9,4 +9,7 @@ for x in a:
     for y in b:
         if x==y:
             match+=1
-print((match/len(b))*100,"% match")
+print(match)
+print(a)
+print(b)
+print(int((match/len(b))*100),"% match")
