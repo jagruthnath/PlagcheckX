f147123bbdca8978b8ac4e830873554a5235a941
jagruthnath <jagruthnath@users.noreply.github.com>
2017-08-22 09:37:08 +0530

    Modules divided.

--- BagOfWords.py
+++ BagOfWords.py
@@ -1,4 +1,4 @@
-import tkinter.filedialog
+"""import tkinter.filedialog
 import tkinter
 
 window = tkinter.Tk()
@@ -24,8 +24,33 @@ b1.grid(row=0,column=2)
 b2.grid(row=1,column=2)
 b3.grid(row=2,column=1)
 
-window.mainloop()
+window.mainloop()"""
 
+def euclid(arg):
+    eucl=0
+    for x in arg:
+        eucl += arg[x] ** 2
+    return eucl
+
+def freq(a,b):
+    d1,d2 = {},{}
+    for x in a:
+        if 123 > ord(x[len(x) - 1]) > 96 or 58 > ord(x[len(x) - 1]) > 47 or ord(x[len(x) - 1]) == 95:
+            s = x
+        else:
+            s = x[:-1]
+        d1[s] = a.count(x)
+        d2[s] = 0
+    for x in b:
+        if 123 > ord(x[len(x) - 1]) > 96 or 58 > ord(x[len(x) - 1]) > 47 or ord(x[len(x) - 1]) == 95:
+            s = x
+        else:
+            s = x[:-1]
+        d2[s] = b.count(x)
+        if s not in d1:
+            d1[s] = 0
+    return [d1,d2]
+pattern = '^\w+$'
 f1=open("Testfiles\/f1.py", "r")
 f2=open("Testfiles\/f2.py", "r")
 str=f1.read().lower()
@@ -33,30 +58,11 @@ str2=f2.read().lower()
 a=str.split()
 b=str2.split()
 match=0
-d1={}
-d2={}
 s=""
-for x in a:
-    if 123 > ord(x[len(x) - 1]) > 96 or 58 > ord(x[len(x) - 1]) > 47 or ord(x[len(x) - 1]) == 95:
-        s = x
-    else:
-        s=x[:-1]
-    d1[s] = a.count(x)
-    d2[s] = 0
-for x in b:
-    if 123 > ord(x[len(x) - 1]) > 96 or 58 > ord(x[len(x) - 1]) > 47 or ord(x[len(x) - 1]) == 95:
-        s = x
-    else:
-        s = x[:-1]
-    d2[s] = b.count(x)
-    if s not in d1:
-        d1[s]=0
-eu1=0
-eu2=0
-for x in d1:
-    eu1 += d1[x]**2
-for x in d2:
-    eu2 += d2[x]**2
+d1=freq(a,b)[0]
+d2=freq(a,b)[1]
+eu1=euclid(d1)
+eu2=euclid(d2)
 dp=0
 for x in d1:
     dp += ( d1[x] * d2[x] )
