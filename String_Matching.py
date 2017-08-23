965d46f9f9133f3fa85431c2a7662700018e7a38
jagruthnath <jagruthnath@users.noreply.github.com>
2017-08-23 13:50:06 +0530

    Adjust indentation

--- String_Matching.py
+++ String_Matching.py
@@ -7,22 +7,18 @@ for filename in glob.glob(os.path.join(path, '*.txt')):
     st.append(open(filename).read().lower())
     a.append(st[i].split())
     i+=1
-
 print(i)
 print(st)
 print(a)
 match=0
 ar=[]
-"""
-for x in range(len(a)):
-    for y in range(len(a[x])):
-        match = 0
-        for z in range(len(a[x])):
-            if a[x][y] == a[x][z]:
-                print(x,y,"   ",x,z)
-                match += 1
-        ar.append(match)
-    del ar[len(ar) - 1]"""
-print(ar)
+
+for x in range(i):
+    for y in range(x+1,i):
+        for m in range(len(a[x])):
+            match = 0
+            for n in range(len(a[y])):
+                if a[x][m] == a[y][n]:
+                    print(x, m, "   ",y, n)
 
 #print(int((match*2/(len(str)+len(str2)))*100),"% match")
