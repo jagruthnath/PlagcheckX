b851849d360e85589e5502679cab4e966c3bd5fb
jagruthnath <jagruthnath@users.noreply.github.com>
2017-08-24 10:01:34 +0530

    String matching improved.

--- String_Matching.py
+++ String_Matching.py
@@ -1,12 +1,10 @@
 import glob
 import re
 import os
-
 lst = []
 directory = {}
 files = []
 path = "Testfiles\/"
-
 for filename in glob.glob(os.path.join(path, '*.txt')):
     #File reading
     f = open(filename,'r')
@@ -26,10 +24,18 @@ def LCS(dct1, dct2):
     a,b = dct1.split(),dct2.split()
     match = 0
     for x in a:
+        if 123 > ord(x[len(x) - 1]) > 96 or 58 > ord(x[len(x) - 1]) > 47 or ord(x[len(x) - 1]) == 95:
+            s = x
+        else:
+            s = x[:-1]
         for y in b:
-            if x == y:
-                if match<len(x):
-                    match = len(x)
+            if 123 > ord(y[len(y) - 1]) > 96 or 58 > ord(y[len(y) - 1]) > 47 or ord(y[len(y) - 1]) == 95:
+                r = y
+            else:
+                r = y[:-1]
+            if s == r:
+                if match<len(s):
+                    match = len(s)
     return int((match*2/(len(dct1)+len(dct2)))*100)
 for i in range(0,(len(files)-1)):
     for j in range((i+1),len(files)):
@@ -37,3 +43,4 @@ for i in range(0,(len(files)-1)):
             print("Plagiarism between ",files[i],"and ",files[j]," is: ",str(LCS(directory[files[i]],directory[files[j]])),"%")
         except:
             print(files[i]," and ",files[j]," are empty.")
+    print()
